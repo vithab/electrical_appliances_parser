3f9194cee010c31376184ad3f32627d9a27f46eb
Vitaly Bogachev <vit.bogachev@gmail.com>
2020-12-06 00:44:43 +0300

    Get appliances urls

--- main.rb
+++ main.rb
@@ -6,6 +6,14 @@ BASE_URL = 'https://www.inortek.ru'
 html = URI.open(BASE_URL)
 document = Nokogiri::HTML(html)
 
-brands = document.css('ul.list-group li.list-group-item a').map { |item| BASE_URL + item['href'] }
+brand_urls = document.css('ul.list-group li.list-group-item a').map { |item| BASE_URL + item['href'] }
 
-p brands
+p brand_urls
+
+html = URI.open(brand_urls.first)
+document = Nokogiri::HTML(html)
+
+appliances_urls = document.css('nav > ul > li > a').map { |item| BASE_URL + item['href'] }
+
+puts "\n\n"
+p appliances_urls
