0e45e66ab92049712e5cac34aecd1cf44c9cab92
Vitaly Bogachev <vit.bogachev@gmail.com>
2020-12-08 17:10:44 +0300

    Refactoring: separation methods to module Appliance

---
+++ lib/appliance.rb
@@ -0,0 +1,14 @@
+module Appliance
+  # Метод для получения html объекта Nokogiri
+  def self.get_html(url)
+    html = open(url) { |result| result.read }
+    Nokogiri::HTML(html)
+  end
+
+  # Метод для записи ссылок категорий оборудования в .txt
+  def self.write_to_txt(url)
+    file_name = url.split('/')[-2]
+
+    File.write("./appliances_urls/#{file_name}.txt", "#{url}\n", mode: 'a')
+  end
+end
--- main.rb
+++ main.rb
@@ -1,36 +1,30 @@
 require 'open-uri'
 require 'nokogiri'
 
+require_relative 'lib/appliance'
+
 BASE_URL = 'https://www.inortek.ru'
 
-def get_html(url)
-  html = open(url) { |result| result.read }
-  Nokogiri::HTML(html)
-end
+document = Appliance.get_html(BASE_URL)
 
-def write_to_txt(url)
-  file_name = url.split('/')[-2]
-
-  File.write("./appliances_urls/#{file_name}.txt", "#{url}\n", mode: 'a')
-end
-
-document = get_html(BASE_URL)
+# Получаем ссылки на страницы брендов
 brand_urls = document.css('ul.list-group li.list-group-item a').map { |item| BASE_URL + item['href'] }
 
 p brand_urls
 
+# Получаем ссылки на страницы категорий товаров брендов
 appliances_urls = 
   brand_urls.map do |url|
-    document = get_html(url)
+    document = Appliance.get_html(url)
     document.css('nav > ul > li > a').map { |item| BASE_URL + item['href'] }
   end
 
 puts "\n\n"
 p appliances_urls
-p appliances_urls.count
 
+# Записываем в отдельный файл ссылки на категории каждого бренда
 appliances_urls.map do |brand|
-  brand.map { |url| write_to_txt(url) }
+  brand.map { |url| Appliance.write_to_txt(url) }
 end
 
 puts "\n\nDone.\n\n"
