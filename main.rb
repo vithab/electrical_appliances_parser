d5db2b64065cf1eeb915d2254e3d8f82af1acd2f
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-01-16 23:24:48 +0300

    Rename variables

--- main.rb
+++ main.rb
@@ -12,20 +12,21 @@ brand_urls = document.css('ul.list-group li.list-group-item a')
               .map { |item| BASE_URL + item['href'] }
 
 p brand_urls
+puts
 
 # Получаем ссылки на страницы категорий товаров брендов
-appliances_urls = 
+category_urls = 
   brand_urls.map do |url|
     document = Appliance.get_html(url)
     document.css('nav > ul > li > a').map { |item| BASE_URL + item['href'] }
   end
 
-puts "\n\n"
-p appliances_urls
-
 # Записываем в отдельный файл ссылки на категории каждого бренда
-appliances_urls.map do |appliance_category|
-  appliance_category.map { |url| Appliance.write_to_txt(url) }
+category_urls.map do |url|
+  url.map do |url|
+    Appliance.write_to_txt(url)
+    p url
+  end
 end
 
 puts "\n\nDone.\n\n"
