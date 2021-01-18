b36f2dfd011de5cf0a63b0b92145c4cf4c87a3c8
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-01-19 01:23:39 +0300

    Check exist html block pagination & get card links per page

--- get_card_product_urls.rb
+++ get_card_product_urls.rb
@@ -2,14 +2,38 @@
 
 require 'open-uri'
 require 'nokogiri'
+require 'byebug'
 
 require_relative 'lib/appliance'
 
 category_urls = 
   File.open("./category_urls/autonics.txt", 'r'){ |file| file.readlines }
-
 category_urls.map! { |link| link.strip }
 
 document = Appliance.get_html(category_urls.first)
-document.css('tbody div.row div.col-xs-6 a')
-  .map { |card_link| p category_urls.first + card_link['href'] }
+page_prefix = '?p='
+total_card_links = []
+
+# Проверка на существование html блока с пагинацией, если есть - берём кол-во
+# страниц, собираем урл с префиксом, собираем ссыоки на карточки с каждой страницы 
+# пагинации
+if Appliance.pagination_valid?(document)
+  page_number = Appliance.get_pagination_number(document)
+  
+  page_number.times do |number|
+    url = category_urls.first + page_prefix + number.to_s
+    page = Appliance.get_html(url)
+    card_links_per_page = page.css('tbody div.row > div.col-xs-6 a')
+      .map { |card_link| category_urls.first + card_link['href'] }
+    
+    total_card_links << card_links_per_page
+  end
+else
+  card_links_per_page = page.css('tbody div.row div.col-xs-6 a')
+    .map { |card_link| category_urls.first + card_link['href'] }
+
+  total_card_links << card_links_per_page
+end
+
+p total_card_links
+p total_card_links.size
