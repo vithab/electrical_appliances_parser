aa751b7ee456f6ce0a83ab62f2979c28fd953768
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-02-10 18:39:05 +0300

    Add main

---
+++ main.rb
@@ -0,0 +1,15 @@
+require_relative 'lib/card'
+
+urls = ['https://www.inortek.ru/autonics/zapchasti_dlya_svetosignalnoj_apparatury/AP_B.html',
+        'https://www.inortek.ru/autonics/absolyutnyj_enkoder/EP50S8_1024_1F_N_24.html']
+
+array_cards = []
+
+urls.map do |url|
+  card = Card.new(url)
+  array_cards << card.get_attributes_card
+  p card.get_attributes_card
+  puts '=' * 80
+end
+
+p array_cards
